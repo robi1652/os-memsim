26bb36027335edd74f91beff4600f0ba8964e265
blakebeanz <78506279+blakebeanz@users.noreply.github.com>
2021-04-27 17:19:23 -0500

    Update mmu.h
    added references to functions used un mmu.cpp

--- include/mmu.h
+++ include/mmu.h
@@ -1,11 +1,14 @@
 #ifndef __MMU_H_
 #define __MMU_H_
 
+#ifndef __MMU_H_
+#define __MMU_H_
+
 #include <iostream>
 #include <string>
 #include <vector>
 
-enum DataType : uint8_t {Char, Short, Int, Float, Long, Double};
+enum DataType : uint8_t {FreeSpace, Char, Short, Int, Float, Long, Double};
 
 typedef struct Variable {
     std::string name;
@@ -32,7 +35,16 @@ public:
     uint32_t createProcess();
     void addVariableToProcess(uint32_t pid, std::string var_name, DataType type, uint32_t size, uint32_t address);
     void print();
+    std::vector<uint32_t> Mmu::mergeAdjacentPartitions(uint32_t pid, uint32_t page_size);
+    int Mmu::getVarVectorLength(int pid);
+    uint32_t Mmu::getVarAddress(Variable* var);
     Variable* Mmu::getVariable(uint32_t pid, std::string v_name);
+    Process* Mmu::getProcess(uint32_t pid);
+    bool Mmu::removeVariable(uint32_t pid, std::string v_name); 
+    Variable* Mmu::getVariableAtIndex(int index, uint32_t pid);
+    uint32_t Mmu::currentSize(int pid);
+    uint32_t Mmu::getLastVarAddress(int pid);    
+    uint32_t Mmu::getLastVarSize(int pid);
 };
 
 #endif // __MMU_H_
