13ddbac2cbf63f51c4807c3796ea388cae7ea31e
blakebeanz <78506279+blakebeanz@users.noreply.github.com>
2021-04-27 17:09:41 -0500

    Update mmu.cpp
    Added getter Functions used in setVariable() and freeVariable() within main.cpp. Corrected syntax errors

--- src/mmu.cpp
+++ src/mmu.cpp
@@ -1,4 +1,7 @@
 #include "mmu.h"
+#include <math.h>
+
+const uint32_t STACK_SIZE = 67108864;
 
 Mmu::Mmu(int memory_size)
 {
@@ -17,6 +20,7 @@ uint32_t Mmu::createProcess()
 
     Variable *var = new Variable();
     var->name = "<FREE_SPACE>";
+    var->type = DataType::FreeSpace;
     var->virtual_address = 0;
     var->size = _max_size;
     proc->variables.push_back(var);
@@ -41,6 +45,7 @@ void Mmu::addVariableToProcess(uint32_t pid, std::string var_name, DataType type
 
     Variable *var = new Variable();
     var->name = var_name;
+    var->type = type;
     var->virtual_address = address;
     var->size = size;
     if (proc != NULL)
@@ -64,52 +69,31 @@ void Mmu::print()
     }
 }
 
-//Process Mmu::getProcessAtIndex(int index) {
-//    return _processes[index];
-//}
-
-int Mmu::getIndexOfProc(int pid) {
+int Mmu::getVarVectorLength(int pid) {
     Process *proc = NULL;
-    int i;
-    for (i = 0; i < _processes.size(); i++) {
+    for (int i = 0; i < _processes.size(); i++) {
         if (_processes[i]->pid == pid) {
-            return i;
+            proc = _processes[i];
+            break;
         }
     }
-    return -1;
-}
-
-int Mmu::getVarVectorLength(int pid) {
-    Process *proc = _processes[getIndexOfProc(pid)];
     return proc->variables.size();
 }
 
-Variable Mmu::getVarAtIndex(int pid, int index) {
-    Process *proc = _processes[getIndexOfProc(pid)]
+Variable* Mmu::getVariableAtIndex(int index, uint32_t pid) {
+    Process *proc;
+    for (int i = 0; i < _processes.size(); i++) {
+        if (_processes[i]->pid == pid) {
+            proc = _processes[i];
+            break;
+        }
+    }
     return proc->variables[index];
 }
 
+
 uint32_t Mmu::getVarAddress(Variable* var) {
     return var->virtual_address;
-}
-
-uint32_t Mmu::getLastVarAddress(int pid) {
-    Process *proc = _processes[getIndexOfProc(pid)];
-    return proc->variables[variables.size() - 1]->virtual_address;
-}
-
-uint32_t Mmu::getLastVarSize(int pid) {
-    Process *proc = _processes[getIndexOfProc(pid)];
-    return proc->variables[variables.size() - 1]->size;
-}
-
-uint32_t Mmu::currentSize(int pid) {
-    int sum = 0;
-    Process *proc = _processes[getIndexOfProc(pid)];
-    for (int i = 0; i < proc->variables.size(); i++) {
-        sum += proc->variables[i]->size;
-    }
-    return sum;
 }
 
 Variable* Mmu::getVariable(uint32_t pid, std::string v_name) {
@@ -130,4 +114,116 @@ Variable* Mmu::getVariable(uint32_t pid, std::string v_name) {
     //should never reach here
     std::cout << "ERROR: logic in Mmu::getDataType did not work as expected. /mmu.cpp line 89" << std::endl;
     return NULL;
+}
+
+
+Process* Mmu::getProcess(uint32_t pid) {
+    Process *p = NULL;
+
+    for (int i = 0; i < _processes.size(); i++) 
+    {
+        if (_processes[i]->pid == pid) return _processes[i];
+    }
+
+    //should never get here
+    return NULL;
+}
+
+
+bool Mmu::removeVariable(uint32_t pid, std::string v_name) 
+{
+    bool variable_removed = false;
+
+    for (int i = 0; i < _processes.size(); i++) 
+    {
+        if (_processes[i]->pid == pid) 
+        {
+            Process *p = _processes[i];
+            for (int j = 0; j < p->variables.size(); j++) 
+            {
+                if (p->variables[j]->name == v_name) {
+                    p->variables[j]->name = "<FREE_SPACE>";
+                    variable_removed = true;
+                    break;
+                }
+            }
+        }
+
+        if (variable_removed) break;
+    }
+
+    return variable_removed;
+}
+
+
+std::vector<uint32_t> Mmu::mergeAdjacentPartitions(uint32_t pid, uint32_t page_size) 
+{
+    Process *p = getProcess(pid);
+
+    int stepper = 0;
+    while (stepper < p->variables.size() - 1)  //-1 to avoid indexing out of vector
+    {
+        std::string v_name = p->variables[stepper]->name;
+        std::string v_next_name = p->variables[stepper + 1]->name;
+        if (v_name == "<FREE_SPACE>" && v_next_name == "<FREE_SPACE>") 
+        {
+            //if there are two adjacent freespaces, comibine their sizes and delete the cell on the right
+            p->variables[stepper]->size += p->variables[stepper+1]->size;
+            p->variables.erase(p->variables.begin()+ stepper + 1); //erase() takes an iterator as parameter
+        } 
+        else //note: stay at this indice until free spaces arent detected
+        {
+            stepper++; 
+        }
+    }
+
+    //now need to adjust page table
+    std::vector<uint32_t> pages_to_delete;
+    for (int i = 0; i < p->variables.size(); i++) 
+    {
+        if (p->variables[i]->name == "<FREE_SPACE>") 
+        {
+            uint32_t start_page = floor((double)p->variables[i]->virtual_address / (double)page_size);
+            uint32_t end_page = floor(((double)p->variables[i]->size + (double)p->variables[i]->virtual_address) / (double) page_size);
+
+            if (start_page + 1 < end_page) 
+            {
+                for (int j = start_page + 1; j < end_page; j++) 
+                {
+                    pages_to_delete.push_back(j);
+                }
+            }
+
+            //edge cases
+            if (p->variables[i]->virtual_address % page_size == 0 && start_page != end_page) 
+            {
+                pages_to_delete.push_back(start_page);
+            }
+            if ((p->variables[i]->virtual_address + p->variables[i]->size) % page_size == 0 && start_page != end_page) 
+            {
+                pages_to_delete.push_back(end_page);
+            }
+        }
+    }
+
+    return pages_to_delete;
+}
+
+uint32_t Mmu::currentSize(int pid) {
+    int sum = 0;
+    Process *proc = getProcess(pid);
+    for (int i = 0; i < proc->variables.size(); i++) {
+        sum += proc->variables[i]->size;
+    }
+    return sum;
+}
+
+uint32_t Mmu::getLastVarAddress(int pid) {
+    Process *proc = getProcess(pid);
+    return proc->variables[proc->variables.size() - 1]->virtual_address;
+}
+
+uint32_t Mmu::getLastVarSize(int pid) {
+    Process *proc = getProcess(pid);
+    return proc->variables[proc->variables.size() - 1]->size;
 }
